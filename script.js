4b73c2795559fe406be256e68ac17a0b11037895
unknown <veorra2019@gmail.com>
2020-12-11 12:58:13 +0300

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+document
+  .querySelector("#submit")
+  .addEventListener("click", (event) => {
+  event.preventDefault();
+  alert(
+    "Спасибо, за нажатие на нашу замечательную кнопку. Значение вашего поля - " +
+      document.querySelector("#text").value
+  );
+});
